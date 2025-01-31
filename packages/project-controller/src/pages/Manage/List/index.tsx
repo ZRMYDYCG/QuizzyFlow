df6c80465f8f50fa8b7a4ff4695d2fa56e70915e
ZRMYDYCG <547471919@qq.com>
2025-01-31 14:23:31 +0800

    feat: 加载添加防抖处理

--- packages/project-controller/src/pages/Manage/List/index.tsx
+++ packages/project-controller/src/pages/Manage/List/index.tsx
@@ -1,17 +1,42 @@
 import QuestionsCard from "./components/QuestionsCard.tsx"
-import { useTitle } from "ahooks"
+import { useTitle, useDebounceFn } from "ahooks"
 import {Spin, Typography} from "antd";
 import ListSearch from "../../../components/list-search.tsx"
-import useLoadQuestionListData from "../../../hooks/useLoadQuestionListData"
-
+import {useEffect, useState} from "react"
+import { useSearchParams } from "react-router-dom"
 
 const { Title } = Typography
 
 const List = () => {
     useTitle("一刻 • 问卷 | 问卷列表")
+    const [searchParams] = useSearchParams()
 
-    const { data = {}, loading } = useLoadQuestionListData()
-    const { list = [] } = data
+    const [list, setList] = useState([]) // 全部的列表数据
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
+    const haveMoreData = total > list.length
+
+    const {run: tryLoadMore} = useDebounceFn(() => {
+        console.log("tryLoadMore")
+    }, { wait: 500 })
+
+    // 页面加载或searchParams变化时, 获取问卷列表数据
+    useEffect(() => {
+        tryLoadMore()
+    }, []);
+
+    // 页面滚动时, 尝试加载更多数据
+    useEffect(() => {
+        if(!haveMoreData) {
+            window.addEventListener("scroll", tryLoadMore)
+        }
+
+        // 组件销毁及searchParams变化之前解绑事件
+        return () => {
+            window.removeEventListener("scroll", tryLoadMore)
+        }
+    }, [searchParams])
 
     return (
         <>
@@ -27,6 +52,7 @@ const List = () => {
             </div>
             {/*问卷列表主体*/}
             <div>
+                <div style={{height: '2000px'}}></div>
                 {loading && <div className="flex justify-center">
                     <Spin />
                 </div>}
