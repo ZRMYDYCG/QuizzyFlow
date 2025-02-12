28a1a2c359d058c31692a89406bb974f6105732f
ZRMYDYCG <547471919@qq.com>
2025-02-12 19:52:38 +0800

    feat: 添加 clsx 和 tailwind-merge 依赖并优化组件生成

--- packages/project-controller/src/pages/Question/Edit/components/components-lib.tsx
+++ packages/project-controller/src/pages/Question/Edit/components/components-lib.tsx
@@ -1,13 +1,26 @@
 import React from "react"
 import { Typography } from "antd"
 import { componentConfigGroup } from "../../components/index"
+import { ComponentConfigType } from "../../components/index"
+
+function gennerateComponent(c: ComponentConfigType) {
+    const { title, type, component: Component } = c
+
+    return <div>
+        <Component />
+    </div>
+}
 
 const ComponentsLib: React.FC = () => {
     return (
         <>
             {componentConfigGroup.map((group, index) => {
-                return <div key={index} className={index > 0 ? 'mt-[20px]' : ''}>
-                    <Typography.Title level={3}>{group.groupName}</Typography.Title>
+                const { groupName, components } = group
+                return <div key={index} className={
+                    index > 0 ? 'mt-[20px]' : ''
+                }>
+                    <Typography.Title level={3}>{groupName}</Typography.Title>
+                    <div key={index}>{components.map((component, index) => gennerateComponent(component))}</div>
                 </div>
             })}
         </>
---
+++ packages/project-controller/src/utils/index.ts
@@ -0,0 +1,9 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * @description 合并多个 className
+ * */ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
