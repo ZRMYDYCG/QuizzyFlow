20acf1af780350c09d157c9b2b4fcc51f4324a3e
ZRMYDYCG <547471919@qq.com>
2025-02-12 15:51:20 +0800

    refactor: 添加组件配置和查找函数

--- packages/project-controller/src/pages/Question/components/index.ts
+++ packages/project-controller/src/pages/Question/components/index.ts
@@ -1,4 +1,26 @@
+import type { FC } from "react"
 import { IQuestionInputProps } from "./question-input"
+import QuestionInputConfig from "./question-input"
 import { IQuestionTitleProps } from "./question-title"
+import QuestionTitleConfig from "./question-title"
 
-export type ComponentPropsType = IQuestionInputProps | IQuestionTitleProps
+// 各组件的 prop 类型
+export type ComponentPropsType = IQuestionInputProps & IQuestionTitleProps
+
+// 组件配置
+export interface ComponentConfigType {
+    title: string
+    type: string
+    component: FC<ComponentPropsType>
+    defaultProps: ComponentPropsType
+}
+
+// 全部组件配置列表
+const componentConfigList: ComponentConfigType[] = [
+    QuestionInputConfig,
+    QuestionTitleConfig,
+]
+
+export function getComponentConfigByType(type: string) {
+    return componentConfigList.find(config => config.type === type)
+}
