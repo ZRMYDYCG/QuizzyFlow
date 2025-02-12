c86751b31bc75636c29e10c647f1a8847435f7e1
ZRMYDYCG <547471919@qq.com>
2025-02-12 20:50:52 +0800

    refactor: 修改组件生成函数并优化渲染结构

--- packages/project-controller/src/pages/Question/Edit/components/components-lib.tsx
+++ packages/project-controller/src/pages/Question/Edit/components/components-lib.tsx
@@ -1,30 +1,35 @@
-import React from "react"
-import { Typography } from "antd"
-import { componentConfigGroup } from "../../components/index"
-import { ComponentConfigType } from "../../components/index"
+import React from "react";
+import { Typography } from "antd";
+import { componentConfigGroup } from "../../components/index";
+import { ComponentConfigType } from "../../components/index";
+import { cn } from "../../../../utils/index";
 
-function gennerateComponent(c: ComponentConfigType) {
-    const { title, type, component: Component } = c
+function generateComponent(c: ComponentConfigType) {
+    const { title, type, component: Component } = c;
 
-    return <div>
-        <Component />
-    </div>
+    return (
+        <div className={cn('mb-[12px] cursor-pointer bg-[#fff] border-[1px] border-[#fff] hover:border-[#e5e5e5] rounded-[4px] p-[12px]')}>
+            <div className="pointer-events-none">
+                <Component />
+            </div>
+        </div>
+    );
 }
 
 const ComponentsLib: React.FC = () => {
     return (
-        <>
+        <div>
             {componentConfigGroup.map((group, index) => {
-                const { groupName, components } = group
-                return <div key={index} className={
-                    index > 0 ? 'mt-[20px]' : ''
-                }>
-                    <Typography.Title level={3}>{groupName}</Typography.Title>
-                    <div key={index}>{components.map((component, index) => gennerateComponent(component))}</div>
-                </div>
+                const { groupName, components } = group;
+                return (
+                    <div key={index} className={cn(index > 0 && 'mt-[20px]')}>
+                        <Typography.Title level={3}>{groupName}</Typography.Title>
+                        {components.map(generateComponent)}
+                    </div>
+                );
             })}
-        </>
-    )
+        </div>
+    );
 }
 
-export default ComponentsLib
+export default ComponentsLib;
