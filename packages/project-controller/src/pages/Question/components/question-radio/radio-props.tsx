158949d9d2f7fad0d91c7e2be7c5c7292f4b2c34
ZRMYDYCG <547471919@qq.com>
2025-02-15 19:31:39 +0800

    feat: 添加了选项的动态增删功能，并确保选项内容不重复。

--- packages/project-controller/src/pages/Question/components/question-radio/radio-props.tsx
+++ packages/project-controller/src/pages/Question/components/question-radio/radio-props.tsx
@@ -1,6 +1,8 @@
 import { FC, useEffect } from 'react'
-import { Form, Radio, Input, Checkbox, Select } from 'antd'
+import { Form, Radio, Input, Checkbox, Select, Button, Space } from 'antd'
 import { IQuestionRadioProps } from './interface.ts'
+import { nanoid } from 'nanoid'
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons'
 
 const RadioProps: FC<IQuestionRadioProps> = (props: IQuestionRadioProps) => {
   const { title, isVertical, value, options = [], onChange, disabled } = props
@@ -11,9 +13,16 @@ const RadioProps: FC<IQuestionRadioProps> = (props: IQuestionRadioProps) => {
   }, [title, value, options, isVertical])
 
   const handleValueChange = () => {
-    if (onChange) {
-      onChange(form.getFieldsValue())
-    }
+    if (onChange === null) return
+    const newValues = form.getFieldsValue()
+    const { options = [] } = newValues
+
+    options.forEach((item: any) => {
+      if (item.value) return
+      item.value = nanoid(5)
+    })
+
+    onChange?.(newValues)
   }
 
   return (
@@ -29,6 +38,59 @@ const RadioProps: FC<IQuestionRadioProps> = (props: IQuestionRadioProps) => {
         rules={[{ required: true, message: '请输入标题' }]}
       >
         <Input />
+      </Form.Item>
+      <Form.Item label="选项">
+        <Form.List name="options">
+          {(fields, { add, remove }) => (
+            <>
+              {/*遍历所有选项*/}
+              {fields.map(({ key, name }, index) => {
+                return (
+                  <Space key={key} align="baseline">
+                    <Form.Item
+                      name={[name, 'text']}
+                      rules={[
+                        { required: true, message: '请输入选项内容' },
+                        {
+                          validator: (_, value) => {
+                            const { options = [] } = form.getFieldsValue()
+
+                            let number = 0
+
+                            options.forEach((item: any) => {
+                              if (item.text === value) {
+                                number++ // 记录相同发的个数
+                              }
+                            })
+                            if (number === 1) return Promise.resolve()
+                            return Promise.reject(new Error('选项内容不能重复'))
+                          },
+                        },
+                      ]}
+                    >
+                      <Input placeholder="请输入选项内容" />
+                    </Form.Item>
+                    {/* 删除按钮 */}
+                    {index > 1 && (
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    )}
+                  </Space>
+                )
+              })}
+              {/*添加选项*/}
+              <Form.Item>
+                <Button
+                  type="link"
+                  onClick={() => add({ text: '', value: '' })}
+                  icon={<PlusOutlined />}
+                  block
+                >
+                  添加选项
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
       </Form.Item>
       <Form.Item label="默认选中" name="value">
         <Select
