db51e745922be02a1d98549d4600aa00ca0d9b3f
ZRMYDYCG <547471919@qq.com>
2025-02-15 20:33:01 +0800

    feat: 增加选项动态添加和删除功能，确保选项内容不重复，并生成唯一值。

--- packages/project-controller/src/pages/Question/components/question-checkbox/checkbox-props.tsx
+++ packages/project-controller/src/pages/Question/components/question-checkbox/checkbox-props.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
-import { Form, Input, Checkbox } from 'antd'
+import { Form, Input, Checkbox, Space, Button } from 'antd'
+import { nanoid } from 'nanoid'
 import { IQuestionCheckboxProps } from './interface.ts'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
 const CheckboxProps: FC<IQuestionCheckboxProps> = (
   props: IQuestionCheckboxProps
@@ -9,8 +11,23 @@ const CheckboxProps: FC<IQuestionCheckboxProps> = (
   const [form] = Form.useForm()
 
   const handleValuesChange = () => {
-    if (onChange) {
+    if (onChange === null) return
+    const newValues = form.getFieldsValue()
+
+    if (newValues.list) {
+      newValues.list = newValues.list.filter(
+        (item: any) => !(item.text === null)
+      )
     }
+
+    const { list = [] } = newValues
+
+    list.forEach((item: any) => {
+      if (item.value) return
+      item.value = nanoid(5)
+    })
+
+    onChange?.(newValues)
   }
   return (
     <Form
@@ -27,7 +44,63 @@ const CheckboxProps: FC<IQuestionCheckboxProps> = (
       >
         <Input />
       </Form.Item>
+      <Form.Item label="选项">
+        <Form.List name="list">
+          {(fields, { add, remove }) => (
+            <>
+              {/*遍历所有选项*/}
+              {fields.map(({ key, name }, index) => {
+                return (
+                  <Space key={key} align="baseline">
+                    {/*当前选项是否选中*/}
+                    <Form.Item name={[name, 'checked']} valuePropName="checked">
+                      <Checkbox />
+                    </Form.Item>
+                    <Form.Item
+                      name={[name, 'text']}
+                      rules={[
+                        { required: true, message: '请输入选项内容' },
+                        {
+                          validator: (_, value) => {
+                            const { list = [] } = form.getFieldsValue()
 
+                            let number = 0
+
+                            list.forEach((item: any) => {
+                              if (item.text === value) {
+                                number++ // 记录相同发的个数
+                              }
+                            })
+                            if (number === 1) return Promise.resolve()
+                            return Promise.reject(new Error('选项内容不能重复'))
+                          },
+                        },
+                      ]}
+                    >
+                      <Input placeholder="请输入选项内容" />
+                    </Form.Item>
+                    {/* 删除按钮 */}
+                    {index > 0 && (
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    )}
+                  </Space>
+                )
+              })}
+              {/*添加选项*/}
+              <Form.Item>
+                <Button
+                  type="link"
+                  onClick={() => add({ text: '', value: '' })}
+                  icon={<PlusOutlined />}
+                  block
+                >
+                  添加选项
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form.Item>
       <Form.Item name="isVertical" valuePropName="checked">
         <Checkbox>竖向排列</Checkbox>
       </Form.Item>
