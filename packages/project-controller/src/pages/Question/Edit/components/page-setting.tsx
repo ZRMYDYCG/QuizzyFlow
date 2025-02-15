80aa7cfa0ee069ea604a4a28744b488c53e3103f
ZRMYDYCG <547471919@qq.com>
2025-02-15 22:59:44 +0800

    feat: 增加页面设置组件并更新右侧面板逻辑

---
+++ packages/project-controller/src/pages/Question/Edit/components/page-setting.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PageSetting: React.FC = () => {
+  return <>页面设置</>
+}
+
+export default PageSetting
--- packages/project-controller/src/pages/Question/Edit/components/right-panel.tsx
+++ packages/project-controller/src/pages/Question/Edit/components/right-panel.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { Tabs } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './components-prop'
+import PageSetting from './page-setting'
+import useGetComponentInfo from '../../../../hooks/useGetComponentInfo'
 
 const RightPanel: React.FC = () => {
+  const [activeKey, setActiveKey] = useState('prop')
+  const { selectedId } = useGetComponentInfo()
+
+  useEffect(() => {
+    if (selectedId) setActiveKey('prop')
+    else setActiveKey('setting')
+  }, [selectedId])
+
   const tabsItems = [
     {
       key: 'prop',
@@ -23,10 +34,10 @@ const RightPanel: React.FC = () => {
           页面设置
         </span>
       ),
-      children: <div>页面设置</div>,
+      children: <PageSetting />,
     },
   ]
-  return <Tabs items={tabsItems} defaultActiveKey="prop"></Tabs>
+  return <Tabs items={tabsItems} activeKey={activeKey}></Tabs>
 }
 
 export default RightPanel
