22970483244b90441c81468d70457940e7e3e2f6
ZRMYDYCG <547471919@qq.com>
2025-02-18 21:36:36 +0800

    feat: 路由全局前缀配置

--- packages/project-server/src/main.ts
+++ packages/project-server/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('/api') // 路由全局前缀
+  await app.listen(8080)
 }
-bootstrap();
+bootstrap()
