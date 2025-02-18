399f564edf912d8831ecb9113ab7f469cc8ab1f8
ZRMYDYCG <547471919@qq.com>
2025-02-18 22:38:21 +0800

    update

---
+++ packages/project-server/src/question/dto/question.dto.ts
@@ -0,0 +1,5 @@
+export class QuestionDto {
+  readonly title: string
+  readonly desc: string
+  // ...
+}
--- packages/project-server/src/question/question.controller.ts
+++ packages/project-server/src/question/question.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Patch,
+  Body,
+} from '@nestjs/common'
+import { QuestionDto } from './dto/question.dto'
 
 @Controller('question')
-export class QuestionController {}
+export class QuestionController {
+  @Get()
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') size: number
+  ) {
+    console.log(keyword, page, size)
+    return {}
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id)
+    return {}
+  }
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    console.log(id, questionDto)
+    return {}
+  }
+}
