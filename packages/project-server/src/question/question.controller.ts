7e152c4409106260bc203f14b1d4c2a7d3b655ea
ZRMYDYCG <547471919@qq.com>
2025-02-19 13:59:04 +0800

    feat: 补充了 service 层操作数据库的操作

--- packages/project-server/src/question/question.controller.ts
+++ packages/project-server/src/question/question.controller.ts
@@ -5,35 +5,55 @@ import {
   Param,
   Post,
   Patch,
+  Delete,
   Body,
   HttpException,
   HttpStatus,
 } from '@nestjs/common'
 import { QuestionDto } from './dto/question.dto'
+import { QuestionService } from './question.service'
 
 @Controller('question')
 export class QuestionController {
+  constructor(private readonly questionService: QuestionService) {}
   @Get('test')
   test() {
     throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
   }
   @Get()
-  findAll(
+  async findAll(
     @Query('keyword') keyword: string,
     @Query('page') page: number,
-    @Query('pageSize') size: number
+    @Query('pageSize') pageSize: number
   ) {
-    console.log(keyword, page, size)
-    return {}
+    const list = await this.questionService.findAllList({
+      keyword,
+      page,
+      pageSize,
+    })
+
+    const count = await this.questionService.count({ keyword })
+
+    return {
+      list,
+      count,
+    }
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
-    console.log(id)
-    return {}
+    return this.questionService.findOne(id)
+  }
+  @Post()
+  create() {
+    return this.questionService.create()
   }
   @Patch(':id')
   updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
-    console.log(id, questionDto)
+    this.questionService.update(id, questionDto)
     return {}
+  }
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return this.questionService.delete(id)
   }
 }
--- packages/project-server/src/question/question.service.ts
+++ packages/project-server/src/question/question.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Question } from './schemas/question.schema'
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+  constructor(
+    // 注入 Question 模型
+    @InjectModel(Question.name) private readonly questionModel
+  ) {}
+  async create() {
+    const question = new this.questionModel({
+      title: 'title',
+      desc: 'desc',
+    })
+    return await question.save()
+  }
+  async findOne(id: string) {
+    return await this.questionModel.findById(id)
+  }
+  async delete(id: string) {
+    return await this.questionModel.findByIdAndRemove(id)
+  }
+  async update(id: string, data) {
+    return await this.questionModel.findByIdAndUpdate({ _id: id }, data)
+  }
+  async findAllList({ keyword = '', page = 1, pageSize = 10 }) {
+    const options: any = {}
+    if (keyword) {
+      const reg = new RegExp(keyword, 'i')
+      options.title = { $regex: reg } // 模糊搜索标题
+    }
+
+    return await this.questionModel
+      .find(options)
+      .sort({ id: -1 }) // 倒序排列
+      // 分页: 跳过前面多少条, 取多少条
+      .skip((page - 1) * pageSize) // 跳过前面多少条
+      .limit(pageSize) // 限制返回多少条
+  }
+
+  async count({ keyword = '' }) {
+    const options: any = {}
+    if (keyword) {
+      const reg = new RegExp(keyword, 'i')
+      options.title = { $regex: reg } // 模糊搜索标题
+    }
+
+    return await this.questionModel.countDocuments(options)
+  }
+}
