b73422c054e80632d0af3a5df5ae59daa19ef880
rimin.zeng <rimin.zeng@yidutech.com>
2025-10-23 10:23:50 +0800

    feat: update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,21 +6,16 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { AnswerModule } from './answer/answer.module';
-import { StatModule } from './stat/stat.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot('mongodb://127.0.0.1:27017/question'),
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
     ),
     QuestionModule,
     UserModule,
     AuthModule,
-    AnswerModule,
-    StatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
