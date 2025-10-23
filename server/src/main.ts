cbf49fa7155f1b17d95160bcb0e1c2d28afb181c
rimin.zeng <rimin.zeng@yidutech.com>
2025-10-23 14:11:29 +0800

    fix: 开启字段校验

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { TransformInterceptor } from './common/interceptors/transform.interceptor'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // 路由全局前缀
   app.setGlobalPrefix('/api')
+  // 全局验证管道
+  app.useGlobalPipes(new ValidationPipe())
   // 全局拦截器
   app.useGlobalInterceptors(new TransformInterceptor())
   // 全局异常过滤器
