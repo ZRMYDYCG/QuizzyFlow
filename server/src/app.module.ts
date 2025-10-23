4223be12120156c7f42a65868f62a4d255dbbbeb
rimin.zeng <rimin.zeng@yidutech.com>
2025-10-23 12:06:01 +0800

    feat: 服务端进行业务架构优化

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { QuestionModule } from './question/question.module'
+import { QuestionModule } from './modules/question/question.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { TransformInterceptor } from './transform/transform.interceptor'
-import { HttpExceptionFilter } from './http-exeption/http-exception.filter'
+import { TransformInterceptor } from './common/interceptors/transform.interceptor'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
